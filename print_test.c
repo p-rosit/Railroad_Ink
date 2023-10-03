aa118ed3ae332430aa9ac75c91bb293fffbfac94
p-rosit <pontus.rosqvist@gmail.com>
2023-10-03 18:13:18 +0200

    removed unused files

--- print_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "./utils/unittests.c"
-
-int main() {
-    char msg[4000];
-
-    unittestp_make_return_message(msg, __func__, "%d: %s", 6, "went.");
-    printf("%s\n", msg);
-}
