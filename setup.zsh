a69a2d971227bf8e61d9a1a5970f6ad495ef1872
p-rosit <pontus.rosqvist@gmail.com>
2023-10-08 18:40:39 +0200

    moved files

--- setup.zsh
+++ setup.zsh
@@ -1,5 +1,6 @@
 root_dir="$(find ~ -type d -name railroad_ink)"
+board_dir=$root_dir/railroad_board
 
 alias inkpile="$root_dir/utils/inkpile.sh"
-alias run_cut="$root_dir/cut/run_cut.sh -a \"$root_dir/railroad_board/board/AOS/*.c $root_dir/railroad_board/data/*.c $root_dir/railroad_board/railroad_expansions.c\""
+alias run_cut="$root_dir/cut/run_cut.sh -a \"$board_dir/board/AOS/*.c $board_dir/data/*.c $board_dir/utils/*.c\""
 
