b74c8e716272b1192dfc30f45b2446d1aeaa3b78
p-rosit <pontus.rosqvist@gmail.com>
2023-10-14 14:50:47 +0200

    started mechanics

---
+++ railroad_board/mechanics/mechanics.h
@@ -0,0 +1,8 @@
+#include "../utils/railroad_types.h"
+
+#include "../board/railroad_board.h"
+
+bool can_place_tile_on_board(board_t, tile_t, tile_load_data_t);
+
+bool place_tile_on_board(board_t, tile_t, tile_load_data_t);
+
